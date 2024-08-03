f25724abbe9d19027089c4ea0e6503c878a20d5e
Bamba-Zoe <muhammedbabasuno998@gmail.com>
2024-08-03 12:16:17 +0000

    Update script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+// JavaScript file example
+
+// Function to display an alert
+function greet() {
+    alert('Hello, welcome to my website!');
+}
+
+// Function to change the text of a paragraph
+function changeText() {
+    document.getElementById('myParagraph').innerHTML = 'Text has been changed!';
+}
